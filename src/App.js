280efcaebbc4c9858ca5fe77e5512ad0b41c04e8
Devashish <Onion4404@gmail.com>
2020-09-12 17:16:18 +0530

    second commit fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import axios from "axios";
-import Theme from './util/theme'
+import Theme from "./util/theme";
 
 // MUI
 import { MuiThemeProvider } from "@material-ui/core";
@@ -15,17 +15,17 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 
 const theme = CreateMuiTheme(Theme);
-const [token, setToken] = useState(token);
-
-useEffect(() => {
-  setToken(localStorage.getItem('FBidToken'));
-}, []);
-
-if (token) {
-  
-}
 
 const App = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("FBidToken"));
+  }, []);
+
+  if (token) {
+  }
+
   return (
     <MuiThemeProvider theme={theme}>
       <div className='App'>
@@ -42,6 +42,6 @@ const App = () => {
       </div>
     </MuiThemeProvider>
   );
-}
+};
 
 export default App;
